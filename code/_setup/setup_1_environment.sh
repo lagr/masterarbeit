d6f01481d05741542cc9a6445a8d95025826586f
Lars Greiving <l_grei02@uni-muenster.de>
2016-01-22 15:55:28 +0100

    Setup changes

--- code/_setup/setup_1_environment.sh
+++ code/_setup/setup_1_environment.sh
@@ -3,6 +3,14 @@
 # This script should setup the docker machines for the example scenario
 # Precondition: Docker, Docker-Compose, Docker Machine and VirtualBox are installed
 
+run_registrator () {
+	docker run -d \
+	    --net=host \
+	    -e "constraint:node==$1" \
+	    --volume=/var/run/docker.sock:/tmp/docker.sock \
+	    gliderlabs/registrator:latest \
+	    $consul_url
+}
 
 #=========== Discovery service machine ===========
 echo "\n\nCreate machine on which the discovery service and the registry will run...\n"
@@ -12,18 +20,18 @@ docker-compose -f ./consul/docker-compose.yml up -d
 docker-compose -p registry -f ../_registry.yml up -d
 
 coordination_machine_ip=$(docker-machine ip coordination-machine)
-coordination_machine_url="consul://$coordination_machine_ip:8500"
+consul_url="consul://$coordination_machine_ip:8500"
 registry_url=$(docker-machine ip coordination-machine):5000
 
 docker rm -f swarm-agent
-docker run -d --name swarm-agent swarm join --advertise $coordination_machine_ip:2376 consul://$coordination_machine_ip:8500
+docker run -d --name swarm-agent swarm join --advertise $coordination_machine_ip:2376 $consul_url
 
 #=========== Development machine ===========
 echo "\n\nCreate machine on which the swarm master and the development services will run..."
 docker-machine create -d virtualbox                        \
 	--swarm --swarm-master                                 \
-	--swarm-discovery="$coordination_machine_url"          \
-	--engine-opt="cluster-store=$coordination_machine_url" \
+	--swarm-discovery="$consul_url"                        \
+	--engine-opt="cluster-store=$consul_url"               \
 	--engine-opt="cluster-advertise=eth1:2376"             \
 	--engine-label edu.proto.machine_env="internal"        \
 	--engine-label edu.proto.ram="big-ram"                 \
@@ -36,8 +44,8 @@ docker-machine create -d virtualbox                        \
 echo "\n\nCreate machine on which the internal enactment will run..."
 docker-machine create -d virtualbox                        \
 	--swarm                                                \
-	--swarm-discovery="$coordination_machine_url"          \
-	--engine-opt="cluster-store=$coordination_machine_url" \
+	--swarm-discovery="$consul_url"                        \
+	--engine-opt="cluster-store=$consul_url"               \
 	--engine-opt="cluster-advertise=eth1:2376"             \
 	--engine-label edu.proto.machine_env="internal"        \
 	--engine-insecure-registry $registry_url               \
@@ -48,28 +56,36 @@ docker-machine create -d virtualbox                        \
 echo "\n\nCreate machine on which the external enactment for wfs with space needs will run..."
 docker-machine create -d virtualbox                        \
 	--swarm                                                \
-	--swarm-discovery="$coordination_machine_url"          \
-	--engine-opt="cluster-store=$coordination_machine_url" \
+	--swarm-discovery="$consul_url"                        \
+	--engine-opt="cluster-store=$consul_url"               \
 	--engine-opt="cluster-advertise=eth1:2376"             \
 	--engine-label edu.proto.machine_env="external"        \
 	--engine-label edu.proto.hdd="big-hdd"                 \
 	--engine-insecure-registry $registry_url               \
 	cloud-machine
 
-docker run -d --name swarm-agent swarm join --advertise=$(docker-machine ip coordination-machine):2376 consul://$(docker-machine ip development-machine):8500
+#docker rm -f swarm-agent
+#docker run -d --name swarm-agent swarm join --advertise $coordination_machine_ip:2376 $consul_url
+#=========== Run service registrators ===========
+eval "$(docker-machine env --swarm development-machine)"
+run_registrator development-machine
+run_registrator internal-machine
+run_registrator cloud-machine
 
 #=========== Show results ===========
-eval "$(docker-machine env --swarm development-machine)"
-
 echo "\n\n Create overlay networks..."
-echo "Create backend network: $(docker network create backend_net)"
-echo "Create frontend network: $(docker network create frontend_net)"
-echo "Create enactment network: $(docker network create enactment_net)"
+echo "Create backend network"
+docker network create backend_net
+echo "Create frontend network"
+docker network create frontend_net
+echo "Create enactment network"
+docker network create enactment_net
 
+sleep 1
 docker network connect backend_net registry_service_1
 docker network connect enactment_net registry_service_1
 
 # show created machines and info on swarm master
 docker info
 echo "\n\n Created machines with the following IPs:"
-docker run --rm swarm list $coordination_machine_url
+docker run --rm swarm list $consul_url
--- code/_setup/setup_2_dev_services.sh
+++ code/_setup/setup_2_dev_services.sh
@@ -2,6 +2,9 @@
 
 # This script should setup the development and administration services
 # Precondition: Environment setup was successfully executed
+eval "$(docker-machine env --swarm development-machine)"
+echo "Started: $(date)\n"
+
 compose='docker-compose --x-networking'
 
 registry_node='coordination-machine'
@@ -9,24 +12,29 @@ development_node='development-machine'
 organization_node='internal-machine'
 logging_node='internal-machine'
 
+registry_url=$(docker-machine ip $registry_node):5000
+
 switch_to () {
   eval "$(docker-machine env ${*})"
 }
 
+build_container () {
+  docker build -t $1 ../$2
+}
+
+build_and_push_container () {
+  build_container $1 $2
+  docker tag -f $1 $registry_url/$1
+  docker push $registry_url/$1
+}
+
 run_container () {
-  echo "---"
-  echo "$1"
-  echo "$2"
-  echo "$3"
-  echo "---"
   REGISTRY=$(docker-machine ip coordination-machine):5000 \
   TARGET_NODE=$1 \
   $compose -p $2 -f ../_$3.yml up -d
 }
 
-
 # =========== pull base images ===========
-#switch_to development-machine
 eval "$(docker-machine env development-machine)"
 
 docker pull cogniteev/echo
@@ -35,50 +43,26 @@ docker pull registry:2
 docker pull postgres
 docker pull ruby:2.2
 
-
 # =========== build service images ===========
-docker build -t "development_app" ../development_app
-
-docker build -t "organization_service" ../organization_service
-docker tag -f organization_service $(docker-machine ip $registry_node):5000/organization_service
-docker push $(docker-machine ip $registry_node):5000/organization_service
+build_container "development_app" development_app
+build_and_push_container "organization_service" organization_service
 
 # =========== launch services ===========
-echo "Build and launch development app..."
-#docker build -t "development_app" ../development_app
+echo "Launch development app..."
+SWARM_MANAGER_IP=$(docker-machine ip development-machine) \
 $compose -p development -f ../_development.yml up -d
 
 eval "$(docker-machine env --swarm development-machine)"
-#switch_to --swarm development-machine
+docker network connect backend_net development_app_1
 
-echo "Build and launch organization service..."
-#docker build -t "organization_service" ../organization_service
-
-#docker tag organization_service $(docker-machine ip $registry_node):5000/organization_service
-#docker push $(docker-machine ip $registry_node):5000/organization_service
-#docker -H $(docker-machine ip $organization_node):2376 pull $(docker-machine ip $registry_node):5000/organization_service
-
-# REGISTRY=$(docker-machine ip $registry_node):5000 \
-# TARGET_NODE=$organization_node \
-# $compose -p organization -f ../_organization.yml up -d
+# === organization service
+echo "Launch organization service..."
 run_container $organization_node organization organization
+sleep 5 && docker exec -it organization_service_1 rake db:migrate:reset db:seed
 
-sleep 5
-docker exec -it organization_service_1 rake db:migrate:reset db:seed
-
-# === Logging service
-# echo "\n\nBuild and launch logging service..."
-# docker build \
-#   --build-arg="constraint:node==$logging_node" \
-#   -t "localhost:5000/logging_service" \
-#   ../logging_service
-
-# docker push localhost:5000/logging_service
-# docker -H $(docker-machine ip $logging_node):2376 pull $(docker-machine ip $registry_node):5000/logging_service
-
+# === logging service
+echo "Launch logging service..."
 run_container $logging_node logging logging
-# REGISTRY=$(docker-machine ip $registry_node):5000 \
-# TARGET_NODE=$logging_node \
-# $compose -p logging -f ../_logging.yml up -d
-
 docker network connect backend_net logging_service_1
+
+echo "\nFinished: $(date)\n"
