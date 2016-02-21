e71d3226a0cf7f4dabdf21b833e113c5baf16077
Lars Greiving <l_grei02@uni-muenster.de>
2016-02-21 16:14:14 +0100

    More implementation text
    Signed-off-by: Lars Greiving <l_grei02@uni-muenster.de>

--- code/definition/app/lib/image_manager.rb
+++ code/definition/app/lib/image_manager.rb
@@ -14,6 +14,12 @@ module ImageManager
 
   private
 
+  def registry_address
+    cluster_store = URI.parse(Docker.info['ClusterStore'])
+    cluster_store.port = 5000
+    cluster_store.select(:host,:port).join(':')
+  end
+
   def publish_image(subject, image)
     type = subject.class.name.underscore.to_sym
     image_name = image_name(type: type, id: subject.id)
